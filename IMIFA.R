656a4a337626a21c741e84af232cc55f2c95832d
Keefe-Murphy <keefe.murphy@ucdconnect.ie>
2015-10-19 22:00:10 +0100

    Created separate script for single case BFA Gibbs sampler, to be read into main results/posterior summaries script. Will later be able to read in full conditional & gibbs scripts for shrinkage case.

--- IMIFA.R
+++ IMIFA.R
@@ -1,6 +1,6 @@
-############################
-# BAYESIAN FACTOR ANALYSIS #
-############################
+######################################################
+### INFINITE MIXTURES OF INFINITE FACTOR ANALYZERS ###
+######################################################
 
 # Preamble
   dataDirectory <- "C:/Users/Windows/Documents/Claire IMIFA"
@@ -16,62 +16,20 @@
   #subjectmarks <- read.csv(paste(dataDirectory,"/Data/","SubjectMarks.csv",sep=""))
   #cereal       <- read.csv(paste(dataDirectory,"/Data/","Cereal.csv",sep=""))
 
-# Simulate data
-  #source(paste(dataDirectory,"/IMIFA-GIT/Simulate_Data.R",sep=""))
-  #save(data, mu.true, f.true, load.true, psi.true, eps.true, file=paste(dataDirectory,"/Data/Simulated_Data.Rdata",sep=""))
-  load(file=paste(dataDirectory,"/Data/Simulated_Data.Rdata",sep=""),envir=.GlobalEnv)
+  # Simulate data
+    #source(paste(dataDirectory,"/IMIFA-GIT/Simulate_Data.R",sep=""))
+    #save(data, mu.true, f.true, load.true, psi.true, eps.true, file=paste(dataDirectory,"/Data/Simulated_Data.Rdata",sep=""))
+    load(file=paste(dataDirectory,"/Data/Simulated_Data.Rdata",sep=""),envir=.GlobalEnv)
 
 # Define full conditional functions
-  source(paste(dataDirectory,"/IMIFA-GIT/BFA_FullConditionals.R",sep=""))
+  source(paste(dataDirectory,"/IMIFA-GIT/IMIFA_FullConditionals.R",sep=""))
 
 # Gibbs Sampler function
-  gibbs  <- function(data, n.iters=100000, Q, sigma.mu=0.5, sigma.l=0.5, psi.alpha=5, psi.beta=5, scaling=T, ...) {
-    
-    # Remove non-numeric columns
-    data      <- data[sapply(data,is.numeric)]
-    
-    # Centre the data (optional)
-    if (scaling) {data <- scale(data, center=T, scale=F)} else  {data <- as.matrix(data)}
-    
-    # Define & initialise variables
-    N         <- nrow(data)
-    P         <- ncol(data)
-    if (Q>=P) stop ("Number of factors must be less than the number of variables")
-    mu        <- matrix(NA, nr=P, nc=n.iters);    rownames(mu)   <- colnames(data)
-    f         <- array(NA, dim=c(N, Q, n.iters)); colnames(f)    <- paste("Factor",1:Q)
-    load      <- array(NA, dim=c(P, Q, n.iters)); rownames(load) <- colnames(data); colnames(load) <- paste("Factor",1:Q)
-    psi       <- matrix(NA, nr=P, nc=n.iters);    rownames(psi)  <- colnames(data)
-    mu.sigma  <- sigma.mu * diag(P)
-    l.sigma   <- sigma.l  * diag(Q)
-    mu.omega  <- matrix(NA, P, P)
-    f.omega   <- matrix(NA, Q, Q)
-    l.omega   <- matrix(NA, Q, Q)
-    mu[,1]    <- mvrnorm(mu=rep(0, P), Sigma=mu.sigma)
-    f[,,1]    <- mvrnorm(n=N, mu=rep(0, Q), Sigma=diag(Q))
-    load[,,1] <- mvrnorm(n=P, mu=rep(0, Q), Sigma=l.sigma)
-    psi[,1]   <- rinvgamma(P, shape=psi.alpha/2, scale=psi.beta/2)
-    
-    # Iterate
-    for(iter in 2:n.iters) { 
-      mu[,iter]       <- sim.mu(mu.sigma, N, P, psi, data, f, load, iter)
-      f.omega         <- sim.omega.f(Q, load, psi, iter)
-      for (i in 1:N) {
-        f[i,,iter]    <- sim.scores(Q, f.omega, load, psi, data, mu, i, iter)
-      }
-      for (j in 1:P) {
-        load[j,,iter] <- sim.load(l.sigma, Q, f, psi, data, mu, j, iter)
-        psi[j,iter]   <- sim.psi(N, psi.alpha, psi.beta, data, mu, load, f, j, iter)
-      }
-    }
-    return(list(mu   = mu,
-                f    = f, 
-                load = load, 
-                psi  = psi))
-  }; gibbs   <- cmpfun(gibbs)
+  source(paste(dataDirectory,"/IMIFA-GIT/Gibbs_BFA_Single.R",sep=""))
 
 # Run the gibbs sampler
   n.iters <- 100000
-  sim     <- gibbs(data=wine, n.iters, Q=3, sigma.mu=0.5, sigma.l=0.5, psi.alpha=5, psi.beta=5)
+  sim     <- gibbs.single(data=wine, n.iters, Q=3, sigma.mu=0.5, sigma.l=0.5, psi.alpha=5, psi.beta=5)
 
 # Save / Load results
   save(sim,file=paste(dataDirectory,"/Simulations/Wine Simulations.Rdata",sep="")) # in server, tick box, export
