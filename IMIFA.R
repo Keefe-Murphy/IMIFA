76a523a3d2f3b24d4c300d0c3c0024ca8210149b
Keefe-Murphy <keefe.murphy@ucdconnect.ie>
2016-01-15 23:31:50 +0000

    Created & edited function to plot loadings heatmaps & moved it to Plotting.Functions.R

--- IMIFA.R
+++ IMIFA.R
@@ -22,7 +22,7 @@
   load(file=paste(dataDirectory, "/Data/Simulated_Data.Rdata", sep=""), envir=.GlobalEnv); if(exists("Label")) rm("Label")
 
 # Initialise the Gibbs Sampler & override default hyperparameters if desired
-  imifa     <- imifa.gibbs(data, method="FA", range.Q=1:2)
+  imifa     <- imifa.gibbs(data, method="IFA")
 
 # Run the Gibbs Sampler
 { Rprof()
@@ -65,47 +65,36 @@
 # Posterior Summaries & Plots, etc.  
   plot.cum.var()
   
-  # Means
-    scatterplot(x=store, y=mu[1,])
-    matplot(t(mu[,]), type="l")
-    plot(post.mu, type="n", main="Posterior Means")
-    text(x=1:P, y=post.mu, names(post.mu), cex=0.5)
-    acf(mu[1,])
+# Means
+  scatterplot(x=store, y=mu[1,])
+  matplot(t(mu[,]), type="l")
+  plot(post.mu, type="n", main="Posterior Means")
+  text(x=1:P, y=post.mu, names(post.mu), cex=0.5)
+  acf(mu[1,])
   
-  # Scores
-    scatterplot(x=store, y=f[1,1,])
-    matplot(t(f[1,,]), type="l")
-    plot(post.f, type="n", main="Posterior Scores")
-    text(post.f[,1], post.f[,2], 1:nrow(post.f), col=if(exists("Label")) as.numeric(Label) else 1, cex=0.5)
-    plot(f[,,length(store)], type="n")
-    text(f[,1,length(store)], f[,2,length(store)], 1:nrow(post.f), col=if(exists("Label")) as.numeric(Label) else 1, cex=0.5)
-    acf(f[1,1,])
+# Scores
+  scatterplot(x=store, y=f[1,1,])
+  matplot(t(f[1,,]), type="l")
+  plot(post.f, type="n", main="Posterior Scores")
+  text(post.f[,1], post.f[,2], 1:nrow(post.f), col=if(exists("Label")) as.numeric(Label) else 1, cex=0.5)
+  plot(f[,,length(store)], type="n")
+  text(f[,1,length(store)], f[,2,length(store)], 1:nrow(post.f), col=if(exists("Label")) as.numeric(Label) else 1, cex=0.5)
+  acf(f[1,1,])
       
-  # Loadings
-    scatterplot(x=store, y=lmat[1,1,])
-    matplot(t(lmat[1,,]), type="l")
-    plot(post.load, type="n")
-    text(post.load[,1], post.load[,2], rownames(post.load), cex=0.5)
-    acf(lmat[1,1,])
-    
-    # Heatmaps
-      #Q <- res$Q
-      par(mfrow=c(1, 1), mar=c(5.1, 7.1, 4.1, 2.1), xpd=F)
-      image(z=t(post.load), xlab="", ylab="", 
-            main="Posterior Loadings", xaxt="n", yaxt="n")
-      axis(1, cex.axis=0.8, line=-0.5, tick=F, 
-           at=if(Q != 1) seq(0, 1, 1/(Q-1)) else 0, labels=1:Q)
-      axis(2, cex.axis=0.5, line=-0.5, tick=F, las=1,
-           at=seq(0, 1, 1/(P-1)), labels=rownames(post.load))
-      box(lwd=2)
-      mtext("Factors", side=1, line=2)
-      if(Q != 1) abline(v=seq(1/(2*(Q-1)), 1-1/(2*(Q-1)), 1/(Q-1)), lty=2, lwd=1)
-      invisible(par(def.par))
+# Loadings
+  scatterplot(x=store, y=lmat[1,1,])
+  matplot(t(lmat[1,,]), type="l")
+  plot(post.load, type="n")
+  text(post.load[,1], post.load[,2], rownames(post.load), cex=0.5)
+  acf(lmat[1,1,])
+  
+# Heatmaps
+  load.heat()
 
-  # Uniquenesses
-    scatterplot(x=store, y=psi[1,])
-    matplot(t(psi[,]), type="l")
-    plot(post.psi, type="n")
-    text(1:length(post.psi), post.psi, names(post.psi), cex=0.5)
-    acf(psi[1,])
+# Uniquenesses
+  scatterplot(x=store, y=psi[1,])
+  matplot(t(psi[,]), type="l")
+  plot(post.psi, type="n")
+  text(1:length(post.psi), post.psi, names(post.psi), cex=0.5)
+  acf(psi[1,])
 ####
--- PlottingFunctions.R
+++ PlottingFunctions.R
@@ -15,3 +15,19 @@
     cat(paste0("Proportion of Variation Explained = ",
                round(prop.exp[length(prop.exp)], 3), "\n"))
   }
+
+# Plot heatmap of Posterior Loadings
+  load.heat     <- function(Q = res$Q) {
+    if(Q > res$Q)  stop("Cannot plot this many loadings columns")
+    par(mfrow=c(1, 1), mar=c(5.1, 7.1, 4.1, 2.1))
+    image(z=t(post.load[,1:Q]), xlab="", ylab="", 
+          main="Posterior Loadings", xaxt="n", yaxt="n")
+    axis(1, cex.axis=0.8, line=-0.5, tick=F, 
+         at=if(Q != 1) seq(0, 1, 1/(Q-1)) else 0, labels=1:Q)
+    axis(2, cex.axis=0.5, line=-0.5, tick=F, las=1,
+         at=seq(0, 1, 1/(P-1)), labels=rownames(post.load))
+    box(lwd=2)
+    mtext("Factors", side=1, line=2)
+    if(Q != 1) abline(v=seq(1/(2*(Q-1)), 1-1/(2*(Q-1)), 1/(Q-1)), lty=2, lwd=1)
+    par(mfrow=c(1, 1), mar=c(5.1, 4.1, 4.1, 2.1))
+  }
