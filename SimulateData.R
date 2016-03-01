b9fc2ad9fad71ab5e594438f153d503d82f094d2
Keefe-Murphy <keefe.murphy@ucdconnect.ie>
2016-03-01 00:56:29 +0000

    Radically overhauled SimulateData.R to simulate grouped data, with different Q values allowed in each group. Stores true means, loadings, uniquenesses, group-specific covariance matrices as lists of length(G), as well as cluster labels.

--- SimulateData.R
+++ SimulateData.R
@@ -2,61 +2,104 @@
 ### Simulate Data (Single & Shrinkage Case) ###
 ###############################################
 
-sim.imifa     <- function(N = 1000, P = 25, Q = 5, G = 1) {
-   
-  # N.old     <- N
-  # G         <- 3
-  # rand      <- sample(1:(N.old - 1), G, replace=F)
-  # N.grp     <- round(rand/sum(rand) * N.old); N <- sum(N.grp)
-  # i in 1:N.grp[1]
-  # i in (N.grp[1] + 1):(N.grp[1] + N.grp[2])
-  # i in (N.grp[1] + N.grp[2] + 1):N
-  if(any(N < 0, P < 0, Q < 0, G <= 0))   stop("N, P, and Q must be strictly non-negative and G must be strictly positive")
-  SimData     <- matrix(0, nr=N, nc=P)
+sim.imifa      <- function(N = 1000, G = 2, P = 25, Q = rep(5, G)) {
+  
+  if(any(N  < 0, P  < 0, Q < 0, G <= 0)) stop("N, P, and Q must be strictly non-negative and G must be strictly positive")
+  if(any(Q >= P, Q >= N - 1))            stop(paste0("Cannot generate this many factors relative to N=", N, " and P=", P))
+  if(length(Q)  != G)                    
+  if(!missing(Q)) {
+   if(length(Q) == 1) {
+    Q    <- rep(Q, G)
+   } else if(length(Q != G)) {           stop(paste0("Q must supplied for each of the G=", G, " groups"))  
+   }                                       
+  }
+  nn     <- rnorm(G, N/G, exp(G * G))
+  if(abs(sum(nn)) < 0.01) nn <- nn + 1
+  nn     <- round(nn / sum(nn) * N)
+  dev    <- N - sum(nn)
+  for(. in seq_len(abs(dev))) {
+    nn[i]       <- nn[i <- sample(G, 1)] + sign(dev)
+  }
+  while(any(nn   < 0)) {
+    neg  <- nn   < 0
+    pos  <- nn   > 0
+    nn[neg][i]  <- nn[neg][i <- sample(sum(neg), 1)] + 1
+    nn[pos][i]  <- nn[pos][i <- sample(sum(pos), 1)] - 1
+  }
+  if(any(nn == 0)) {
+    G    <- G - sum(nn == 0)
+    Q    <- Q[1:length(G)]
+    nn   <- nn[nn > 0]
+  }
+  
+  gnames        <- paste0("Group ", 1:G)
+  vnames        <- paste0("Var ", 1:P)
+  SimData       <- matrix(0, nr=0, nc=P)
+  true.mu       <- setNames(vector("list", G), gnames) #matrix(0, nr=P, nc=G)
+  true.l        <- setNames(vector("list", G), gnames)
+  true.psi      <- setNames(vector("list", G), gnames)
+  true.cov      <- setNames(vector("list", G), gnames)
+  true.zlab     <- rep(0, N)
   
 # Simulate true parameter values
-  U.mu        <- sqrt(max(Q, 1) * diag(P))
-  z.mu        <- rnorm(P, 0, 1)
-  v.mu        <- U.mu %*% z.mu
-  mu.mu       <- rnorm(P, 0, max(Q, 1))
-  mu.true     <- as.vector(mu.mu + v.mu)
+  for(g in 1:G) {
+    Q.g         <- Q[g]
+    U.mu        <- sqrt(abs(rnorm(P, Q.g, g)) * diag(P))
+    z.mu        <- rnorm(P, 0, 1)
+    v.mu        <- U.mu %*% z.mu
+    mu.mu       <- rnorm(P, 0, max(Q.g, 1))
+    mu.true     <- as.vector(mu.mu + v.mu)
     
-  if(Q > 0) {
-    U.load    <- sqrt(Q * diag(Q))
-    z.load    <- matrix(rnorm(P * Q, 0, 1), nr=Q, ncol=P)
-    v.load    <- t(U.load %*% z.load)
-    mu.load   <- rnorm(Q, 0, Q/2)
-    load.true <- mu.load + v.load
-  } else {
-    load.true <- matrix(, nr=P, nc=0)
-  }
+    if(Q.g > 0) {
+      U.load    <- sqrt(abs(rnorm(Q.g, 0, g/2)) * diag(Q.g))
+      z.load    <- matrix(rnorm(P * Q.g, 0, 1), nr=Q.g, ncol=P)
+      v.load    <- t(U.load %*% z.load)
+      mu.load   <- rnorm(Q.g, 0, Q.g/2)
+      l.true    <- mu.load + v.load
+    } else {
+      l.true    <- matrix(, nr=P, nc=0)
+    }
   
-  psi.true    <- 1/rgamma(n=P, shape=4, rate=1)                            
+    shape.g     <- floor((g * max(Q.g, 2))/2)
+    psi.true    <- 1/rgamma(n=P, shape=shape.g, rate=shape.g - 1)                            
     
-  names(mu.true)      <- c(1:P)
-  if(Q > 0) colnames(load.true) <- paste("Factor", 1:Q)
-  rownames(load.true) <- c(1:P)
-  names(psi.true)     <- c(1:P)
-  
-# Simulate data
-  omega       <- tcrossprod(load.true, load.true) + diag(psi.true)
-  if(Q > 0) {
-    U.om      <- chol(omega)
-  } else {
-    U.om      <- sqrt(omega)
+  # Simulate data
+    covmat      <- tcrossprod(l.true, l.true) + diag(psi.true)
+    if(Q.g > 0) {
+      U.om      <- chol(covmat)
+    } else {
+      U.om      <- sqrt(covmat)
+    }
+    z           <- matrix(rnorm(P * nn[g], 0, 1), nr=P, ncol=nn[g])
+    var         <- U.om %*% z
+    SimData     <- rbind(SimData, t(rep(mu.true, nn[g]) + var))
+    names(mu.true)      <- vnames
+    names(psi.true)     <- vnames
+    dimnames(covmat)    <- list(vnames, vnames)
+    dimnames(l.true)    <- list(vnames, if(Q.g > 0) paste0("Factor ", 1:Q.g))
+    true.mu[[g]]        <- mu.true
+    true.l[[g]]         <- l.true
+    true.psi[[g]]       <- psi.true
+    true.cov[[g]]       <- covmat
   }
-  z           <- matrix(rnorm(P * N, 0, 1), nr=P, ncol=N)
-  v           <- U.om %*% z
-  SimData     <- t(mu.true + v)
   
 # Post-process data
-  SimData     <- SimData[sample(1:N, N, replace=F),]
-  rownames(SimData)   <- c(1:N)
-  colnames(SimData)   <- c(1:P)
-  SimData     <- as.data.frame(SimData)
-  attr(SimData, "mu.true")      <- mu.true
-  attr(SimData, "load.true")    <- load.true
-  attr(SimData, "psi.true")     <- psi.true
-  class(SimData)      <- c("data.frame", "IMIFA")
+  true.zlab     <- factor(rep(nn, nn), labels=1:length(nn))
+  permute       <- sample(1:N, N, replace=F)
+  SimData       <- SimData[permute,]
+  true.zlab     <- true.zlab[permute]
+  dimnames(SimData)     <- list(paste0("Obs ", 1:N), vnames)
+  SimData       <- as.data.frame(SimData)
+  attr(SimData, 
+       "Means")         <- true.mu
+  attr(SimData, 
+       "Loadings")      <- true.l
+  attr(SimData, 
+       "Uniquenesses")  <- true.psi
+  attr(SimData, 
+       "Labels")        <- true.zlab
+  attr(SimData,
+       "Covariance")    <- true.cov
+  class(SimData)        <- c("data.frame", "IMIFA")
   return(SimData)
 }
