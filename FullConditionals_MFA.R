6f4f1fff3d05667e69e45362ee2cb1d8c66215c0
Keefe-Murphy <keefe.murphy@ucdconnect.ie>
2016-03-15 10:14:59 +0000

    Copied in FullConditionals_MFA.R & Gibbs_MFA.R.

---
+++ FullConditionals_MFA.R
@@ -0,0 +1,91 @@
+#############################################
+### IMIFA Full Conditionals (Single Case) ###
+#############################################
+                         
+# Means
+  sim.mu        <- function(N = NULL, P = NULL, sigma.mu = NULL, psi.inv = NULL, 
+                            sum.data = NULL, sum.f = NULL, lmat = NULL, ...) {
+    mu.omega    <- 1/(sigma.mu + N * psi.inv)
+    U.mu        <- sqrt(mu.omega)
+    z.mu        <- rnorm(P, 0, 1)
+    v.mu        <- U.mu * z.mu
+    mu.mu       <- mu.omega * (psi.inv * (sum.data - lmat %*% sum.f))
+      as.vector(mu.mu + v.mu)
+  }
+
+# Scores
+  sim.scores    <- function(N = NULL, Q = NULL, lmat = NULL,
+                            psi.inv = NULL, c.data = NULL, ...) {
+    load.psi    <- lmat * psi.inv
+    f.omega.a   <- diag(Q) + crossprod(load.psi, lmat)
+    U.f         <- chol(f.omega.a)
+    f.omega.b   <- load.psi %*% chol2inv(U.f)
+    z.f         <- matrix(rnorm(Q * N, 0, 1), nr=Q, ncol=N)
+    v.f         <- backsolve(U.f, z.f)
+    mu.f        <- c.data %*% f.omega.b
+      mu.f + t(v.f)
+  }
+
+# Loadings
+  sim.load      <- function(l.sigma = NULL, Q = NULL, c.data.j = NULL, 
+                            f = NULL, psi.inv.j = NULL, FtF = NULL, ...) {
+    l.omega     <- l.sigma + psi.inv.j * FtF
+    U.load      <- chol(l.omega)
+    z.load      <- rnorm(Q, 0, 1)
+    v.load      <- backsolve(U.load, z.load)
+    mu.load     <- psi.inv.j * chol2inv(U.load) %*% crossprod(f, c.data.j)
+      mu.load + v.load
+  }
+
+# Uniquenesses
+  sim.psi.inv   <- function(N = NULL, P = NULL, psi.alpha = NULL, psi.beta = NULL, 
+                            c.data = NULL, f = NULL, lmat = NULL, ...) { 
+    rate.t      <- c.data - tcrossprod(f, lmat)
+    rate.t      <- colSums(rate.t * rate.t)
+      rgamma(P, shape=(N + psi.alpha)/2, 
+                rate=(rate.t + psi.beta)/2) 
+  }
+
+# Priors
+  # Means
+    sim.mu.p    <- function(P = NULL, sigma.mu = NULL, ...) {
+      U.mu      <- sqrt(1/sigma.mu)
+      z.mu      <- rnorm(P, 0, 1)
+        U.mu * z.mu
+    }
+  
+  # Scores
+    sim.f.p     <- function(Q = NULL, N = NULL, ...) {
+        matrix(rnorm(N * Q, 0, 1), nr=N, ncol=Q)
+    }
+
+  # Loadings
+    sim.l.p     <- function(Q = NULL, P = NULL, sigma.l = NULL, ...) {
+      U.l       <- sqrt(1/sigma.l)
+      z.l       <- matrix(rnorm(P * Q, 0, 1), nr=P, ncol=Q)
+        z.l * U.l
+    }
+
+  # Uniquenesses
+    sim.pi.p    <- function(P = NULL, psi.alpha = NULL, psi.beta = NULL, ...) {
+        rgamma(n=P, shape=psi.alpha/2, rate=psi.beta/2) 
+    }
+
+  # Multivariate Normal Density
+    mvdnorm     <- function(data = NULL, mu = NULL, 
+                            Sigma = NULL, P = NULL, pie = 1) {
+      if(all(Sigma[!diag(P)] == 0)) {
+        U.Sig   <- sqrt(Sigma)
+      } else {
+        U.Sig   <- chol(Sigma)
+      }
+      rooti     <- backsolve(U.Sig, diag(P))
+      quad      <- crossprod(rooti, t(data) - mu)
+      quads     <- colSums(quad * quad)
+        pie * exp(- P/2 * log(2 * pi) + sum(log(diag(rooti))) - 0.5 * quads)
+    }
+  
+  # BIC
+    bic.mcmc    <- function(like = NULL, pen = NULL, ...) {
+        2 * sum(log(like)) - pen
+    }
---
+++ Gibbs_MFA.R
@@ -0,0 +1,116 @@
+################################################################
+### Gibbs Sampler for Bayesian Factor Analysis (Single Case) ###
+################################################################
+  
+# Gibbs Sampler Function
+  gibbs.FA       <- function(Q, data, n.iters, N, P, 
+                             sigma.mu, psi.alpha, psi.beta,
+                             burnin, thinning, n.store,
+                             verbose, sw, sigma.l, ...) {
+        
+  # Define & initialise variables
+    cnames       <- colnames(data)
+    rnames       <- rownames(data)
+    facnames     <- paste0("Factor ", 1:Q)
+    iternames    <- paste0("Iteration", 1:n.store)
+    if(sw["mu.sw"]) {
+      mu.store   <- matrix(0, nr=P, nc=n.store)
+      dimnames(mu.store)   <- list(cnames, iternames)
+    }
+    if(sw["f.sw"])  {
+      f.store    <- array(0, dim=c(N, Q, n.store))
+      dimnames(f.store)    <- list(rnames, if(Q > 0) facnames, iternames)
+    }
+    if(sw["l.sw"])  {
+      load.store <- array(0, dim=c(P, Q, n.store))
+      dimnames(load.store) <- list(cnames, if(Q > 0) facnames, iternames)
+    }
+    if(sw["p.sw"])  {
+      psi.store  <- matrix(0, nr=P, nc=n.store)
+      dimnames(psi.store)  <- list(cnames, iternames)
+    }
+    post.mu      <- setNames(rep(0, P), cnames)
+    post.psi     <- setNames(rep(0, P), cnames)
+    post.Sigma   <- matrix(0, nr=P, nc=P)
+    bicM         <- -Inf
+    K            <- P * Q - 0.5 * Q * (Q - 1) + 2 * P
+    pen          <- K * log(N)
+    cov.emp      <- cov(data)
+    dimnames(post.Sigma)   <- list(cnames, cnames)
+    dimnames(cov.emp)      <- dimnames(post.Sigma)
+    
+    sigma.mu     <- 1/sigma.mu
+    sigma.l      <- 1/sigma.l
+    mu           <- sim.mu.p(P=P, sigma.mu=sigma.mu)  
+    f            <- sim.f.p(Q=Q, N=N)
+    lmat         <- sim.l.p(Q=Q, P=P, sigma.l=sigma.l)
+    psi.inv      <- sim.pi.p(P=P, psi.alpha=psi.alpha, psi.beta=psi.beta)
+    l.sigma      <- sigma.l * diag(Q)
+    sum.data     <- colSums(data)
+  
+  # Iterate
+    for(iter in 2:n.iters) { 
+      if(verbose) {
+        if(all(iter < burnin, iter %% (burnin/10) == 0)) {
+          cat(paste0("Iteration: ", iter, "\n"))
+        } else if(iter %% (n.iters/10) == 0) {
+          cat(paste0("Iteration: ", iter, "\n"))
+        }
+      }
+      
+    # Means
+      sum.f      <- colSums(f)
+      mu         <- sim.mu(N=N, P=P, sigma.mu=sigma.mu, psi.inv=psi.inv,
+                           sum.data=sum.data, sum.f=sum.f, lmat=lmat)
+    
+    # Scores
+      c.data     <- sweep(data, 2, mu, FUN="-")
+      if(Q > 0) {
+        f        <- sim.scores(N=N, Q=Q, lmat=lmat, psi.inv=psi.inv, c.data=c.data)
+      } else {
+        f        <- matrix(, nr=N, nc=0)
+      }
+                
+    # Loadings
+      FtF        <- crossprod(f)
+      if(Q > 0) {
+        for (j in 1:P) {
+          psi.inv.j <- psi.inv[j]
+          c.data.j  <- c.data[,j]
+          lmat[j,]  <- sim.load(l.sigma=l.sigma, Q=Q, c.data.j=c.data.j, 
+                                f=f, psi.inv.j = psi.inv.j, FtF=FtF)
+        }
+      } else {
+        lmat     <- matrix(, nr=P, nc=0)
+      }
+      
+    # Uniquenesses
+      psi.inv    <- sim.psi.inv(N=N, P=P, psi.alpha=psi.alpha, psi.beta=psi.beta,
+                                c.data=c.data, f=f, lmat=lmat)
+    
+      if(all(iter > burnin, iter %% thinning == 0)) {
+        new.iter <- ceiling((iter - burnin)/thinning)
+        psi      <- 1/psi.inv
+        if(sw["mu.sw"]) mu.store[,new.iter]    <- mu  
+        if(sw["f.sw"])  f.store[,,new.iter]    <- f
+        if(sw["l.sw"])  load.store[,,new.iter] <- lmat
+        if(sw["p.sw"])  psi.store[,new.iter]   <- psi
+        post.mu     <-  post.mu + mu/n.store
+        post.psi    <-  post.psi + psi/n.store
+        Sigma       <-  tcrossprod(lmat) + diag(psi)
+        post.Sigma  <-  post.Sigma + Sigma/n.store
+        like        <-  mvdnorm(data=data, mu=mu, Sigma=Sigma, P=P)
+        bicM        <-  max(bicM, bic.mcmc(like=like, pen=pen), na.rm=T)
+      }  
+    }
+    returns   <- list(mu   = if(sw["mu.sw"]) mu.store,
+                      f    = if(sw["f.sw"])  f.store, 
+                      load = if(sw["l.sw"])  load.store, 
+                      psi  = if(sw["p.sw"])  psi.store,
+                      cov.mat    = cov.emp,
+                      post.mu    = post.mu,
+                      post.psi   = post.psi,
+                      post.Sigma = post.Sigma,
+                      bic        = bicM)
+    return(returns[!sapply(returns, is.null)])
+  }
