93c19a11eca8c9c45cb6433d1e5bb39f0547e76f
Keefe-Murphy <keefe.murphy@ucdconnect.ie>
2016-10-18 14:20:47 +0100

    Significantly edited sim.IMIFA() function: new defaults, added ability to specify pis rather than nn (must be of length G and sum to 1) which simulate labels inside a while loop, various cosmetic changess, and improvements to the generation of 'true' parameter values.

--- SimulateData.R
+++ SimulateData.R
@@ -2,32 +2,31 @@
 ### Simulate Data (Single & Shrinkage Case) ###
 ###############################################
 
-sim.IMIFA      <- function(N = 500, G = 3, P = 25, Q = rep(5, G), nn = NULL) {
+sim.IMIFA      <- function(N = 300, G = 3, P = 50, Q = rep(4, G), pis = rep(1/G, G), nn = NULL) {
   
-  if(any(N  < 0, P  < 0, Q < 0, G <= 0)) stop("N, P, and Q must be strictly non-negative and G must be strictly positive")
+  if(any(N  < 0, P  < 0, Q < 0, G <= 0)) stop("'N', 'P', and 'Q' must be strictly non-negative and 'G' must be strictly positive")
   if(any(Q >= P, Q >= N - 1))            stop(paste0("Cannot generate this many factors relative to N=", N, " and P=", P))
-  if(length(Q) != G)                    
+  if(length(Q) != G) {                   
     if(!missing(Q))  {
-      if(length(Q) == 1)  {
+      if(length(Q) == 1) {
         Q      <- rep(Q, G)
-      } else if(length(Q != G))  {       stop(paste0("Q must supplied for each of the G=", G, " groups"))  
-      }                                       
+      } else if(length(Q != G))          stop(paste0("'Q' must supplied for each of the G=", G, " groups"))  
     }
-  if(!missing(nn)) {
+  }
+  if(!missing(nn) && missing(pi.prop)) {
     nn         <- as.integer(nn)
-    if(any(length(nn) != G, 
-           sum(nn)    !=N))              stop(paste0("nn must be an integer vector of length ", G, " which sums to ", N))
+    if(any(nn  == 0))                    stop("All 'nn' values be strictly positive; simulating empty groups not allowed")
+    if(any(length(nn)  != G, 
+           sum(nn)     != N))            stop(paste0("'nn' must be an integer vector of length G=", G, " which sums to N=", N))
   } else {
-    labs       <- factor(which(rmultinom(N, size=1, prob=rdirichlet(1, rep(0.5, G))) != 0, arr.ind=TRUE)[,1], levels=seq_len(G))
-    nn         <- tabulate(labs, nbins=G)
+    if(any(length(pis) != G,
+           sum(pis)    != 1))            stop(paste0("'pis' must be a vector of length G=", G, " which sums to ", 1))
+    nn         <- rep(0,  G)
+    while(any(nn < floor(N/(G * G))))  {
+      labs     <- factor(which(rmultinom(N, size=1, prob=pis) != 0, arr.ind=TRUE)[,1], levels=seq_len(G))
+      nn       <- tabulate(labs, nbins=G)  
+    }
   } 
-
-  if(sum(nn == 0) > 0) {
-    nn.ind     <- which(nn == 0)
-    nn.max     <- which.max(nn)
-    nn[nn.ind] <- nn[nn.ind] + 1
-    nn[nn.max] <- nn[nn.max] - length(nn.ind)
-  }                           
                                               
   vnames        <- paste0("Var ", seq_len(P))
   simdata       <- matrix(0, nr=0, nc=P)
@@ -37,23 +36,21 @@ sim.IMIFA      <- function(N = 500, G = 3, P = 25, Q = rep(5, G), nn = NULL) {
   true.cov      <- true.mu
   
 # Simulate true parameter values
-  for(g in seq_len(G)) {
+  for(g in seq_len(G))   {
     Q.g         <- Q[g]
-    mu.true     <- as.vector(rnorm(P, 0, max(Q.g, 1)) + sqrt(abs(rnorm(P, Q.g, g)) * diag(P)) %*% rnorm(P, 0, 1))
-    
+    mu.true     <- rnorm(P, rnorm(P, sample(-5:5, P, replace=TRUE), 1), 2)
     if(Q.g > 0) {
-      l.true    <- rnorm(Q.g, 0, Q.g/2) + t(sqrt(abs(rnorm(Q.g, 0, g/2)) * diag(Q.g)) %*% matrix(rnorm(P * Q.g, 0, 1), nr=Q.g, ncol=P))
+      l.true    <- matrix(rnorm(P * Q.g, 0, 1), nr=P, nc=Q.g)
     } else {
       l.true    <- matrix(, nr=P, nc=0)
     }
-  
-    shape.g     <- floor((g * max(Q.g, 2))/2)
-    psi.true    <- 1/rgamma(n=P, shape=shape.g, rate=shape.g - 1)                            
+    psi.true    <- runif(P, 0, 0.5)
     
   # Simulate data
     covmat      <- tcrossprod(l.true, l.true) + diag(psi.true)
+    if(!isSymmetric(covmat))             stop("Non-symmetric covariance matrix")
     sigma       <- if(Q.g > 0) chol(covmat) else sqrt(covmat)
-    simdata     <- rbind(simdata, t(rep(mu.true, nn[g]) + sigma %*% matrix(rnorm(P * nn[g], 0, 1), nr=P, ncol=nn[g])))
+    simdata     <- rbind(simdata, t(rep(mu.true, nn[g]) + sigma %*% matrix(rnorm(P * nn[g], 0, 1), nr=P, nc=nn[g])))
     names(mu.true)      <- vnames
     names(psi.true)     <- vnames
     dimnames(covmat)    <- list(vnames, vnames)
